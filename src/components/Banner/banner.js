9b2287a1c011f50ed41e7314706c4e02d4ea703d
Ítalo Magno <italomagnov@gmail.com>
2022-10-26 10:55:32 -0400

    iniciando o curso de react. Criado o primeiro componente: Banner

---
+++ src/components/Banner/banner.js
@@ -0,0 +1,11 @@
+import './banner.css'
+
+function Banner() {
+    return (
+        <header className='banner'>
+            <img src='images/banner.png' alt='O banner principal da página do Organo'></img>
+        </header>
+    );
+}
+
+export default Banner
