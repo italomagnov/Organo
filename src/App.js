491d14821b60548887b8c8596c6ec0e29a081b7b
Ítalo Magno <italomagnov@gmail.com>
2022-10-26 14:30:33 -0400

    criação dos inputs de text

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import Banner from './components/Banner/banner';
+import Banner from './components/Banner/';
+import CampoTexto from './components/CampoTexto/';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <CampoTexto label='Nome' placeholder='Digite seu nome' />
+      <CampoTexto label='Cargo' placeholder='Digite seu cargo' />
+      <CampoTexto label='Imagem' placeholder='Informe o endereço da imagem' />
+
     </div>
   );
 }
--- src/components/Banner/banner.js
+++ src/components/Banner/banner.js
@@ -1,11 +1,9 @@
-import './banner.css'
+import './Banner.css'
 
-function Banner() {
+export const Banner = () => {
     return (
         <header className='banner'>
             <img src='images/banner.png' alt='O banner principal da página do Organo'></img>
         </header>
     );
 }
-
-export default Banner
